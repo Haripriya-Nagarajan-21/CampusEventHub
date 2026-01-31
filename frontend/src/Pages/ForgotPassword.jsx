a3a1360d0e843626fa144bdd930ca20d8a42d60b
Haripriya Nagarajan <haripriya0521@gmail.com>
2026-02-01 00:57:19 +0530

    Update ForgotPassword.jsx

--- frontend/src/Pages/ForgotPassword.jsx
+++ frontend/src/Pages/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/ForgotPassword.css";
+import "../Styles/ForgotPassword.css";
 import forgotPasswordIllustration from "../assets/Forgot password-pana 1.svg";
 import { notifyError, notifySuccess } from "../utils/toast";
 
