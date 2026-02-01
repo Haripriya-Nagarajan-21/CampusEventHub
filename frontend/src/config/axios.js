9e5a32d944bfaee87368315cbcb2e57adb77e746
Haripriya-Nagarajan-21 <haripriya0521@gmail.com>
2026-02-01 19:01:15 +0530

    update

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
+
 console.log("API URL:", import.meta.env.VITE_API_URL); 
+
+
+
+
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
   timeout: 5000,
@@ -8,23 +13,5 @@ const api = axios.create({
   },
   withCredentials: true,
 });
-
-// Response interceptor
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.code === "ECONNABORTED") {
-      return Promise.reject({
-        message: "Server is not responding. Please try again later.",
-      });
-    }
-    if (!error.response) {
-      return Promise.reject({
-        message: "Network error. Please check your internet connection.",
-      });
-    }
-    return Promise.reject(error.response.data);
-  }
-);
 
 export default api;
