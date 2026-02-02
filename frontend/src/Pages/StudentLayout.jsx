886997172741993a73d8d80edb9229bd09307d70
Haripriya Nagarajan <haripriya0521@gmail.com>
2026-02-02 16:02:23 +0530

    Update StudentLayout.jsx

--- frontend/src/Pages/StudentLayout.jsx
+++ frontend/src/Pages/StudentLayout.jsx
@@ -170,11 +170,11 @@ const StudentLayout = ({ children }) => {
 
       {/* Navbar */}
       <main className="main-content">
-        <header className="topbar">
-          <button className="menu-icon" onClick={toggleSidebar}>☰</button>
-
-          <h2 className="student-title">Student Dashboard</h2>
-
+       <header className="topbar">
+  <div className="topbar-left">
+    <button className="menu-icon" onClick={toggleSidebar}>☰</button>
+    <h2 className="student-title">Student Dashboard</h2>
+  </div>
           <div className="right-controls">
 
             {/* Dark Mode */}
