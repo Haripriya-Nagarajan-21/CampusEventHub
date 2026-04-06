e88a2399720790d4ca4b11f349cab9f0752bb409
Haripriya Nagarajan <haripriya0521@gmail.com>
2026-04-06 15:06:10 +0530

    Update registrationController.js

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -24,12 +24,14 @@ cloudinary.v2.config({
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // ✅ important
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: `"Campus Event Portal" <${process.env.EMAIL_USER}>`,
